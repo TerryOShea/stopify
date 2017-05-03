b19e184affc1ace939f54dde57024a25f2ed4218
Terry O'Shea <terrykoshea@gmail.com>
2017-05-03 09:42:57 -0700

    create most of header

---
+++ frontend/components/hero/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Hero = () => (
+  <section className="hero-gradient">
+    <h2>Music for anybody.</h2>
+    <section className="hero-btns">
+      <button type="button" className="stopify-free-btn">Get Stopify Free</button>
+      <button type="button" className="stopify-premium-btn">Get Stopify Premium</button>
+    </section>
+  </section>
+);
+
+export default Hero;
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../header/header';
+import Hero from '../hero/hero';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 };
