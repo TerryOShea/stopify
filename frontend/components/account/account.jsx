5dc7d553374cbaa309647ef27fb281d514fc1ae6
Terry O'Shea <terrykoshea@gmail.com>
2017-05-13 20:03:44 -0700

    work on account page

--- frontend/components/account/account.jsx
+++ frontend/components/account/account.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Hero from './hero/hero';
+import Sidebar from './sidebar/sidebar';
 
 const Account = () => (
   <div>
     <Header />
     <section className="account-main">
       <Hero />
+      <Sidebar />
     </section>
     <Footer />
   </div>
